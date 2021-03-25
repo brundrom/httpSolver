8c141fb4eca4f0f76f1e7ae333382f2ae639350e
Gennady Petrov <brundrom@gmail.com>
2021-03-25 15:16:43 +0300

    Successful post request to server and getting help message

--- src/main/scala/SolverLevelOne.scala
+++ src/main/scala/SolverLevelOne.scala
@@ -1,11 +1,18 @@
-import scalaj.http.{Http, Token}
+import scalaj.http.{Http, HttpRequest, HttpResponse, Token}
 
 object SolverLevelOne extends App {
   println("Scala task solver for Daniel Server v.0.1A")
 
-  val response = Http("http://34.122.17.54:8080/api/sign-up")
-    .postData("{ \"name\": \"Gennady\" }")
+  val token = "28076f4c-a6a8-4379-ae38-6fa4ca9b2125"
+
+  //val response = Http("http://34.122.17.54:8080/api/sign-up")
+  //  .postData("{ \"name\": \"Gennady\" }")
+  //  .asString
+  //println("My token is " + response)
+
+  val response = Http("http://34.122.17.54:8080/api/task/help")
+    .header("puzzle_session", token)
     .asString
-  println("My token is " + response)
+  println("My task is " + response)
 
 }
